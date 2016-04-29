52da9da9ff84e66de4c000bfccaf311fb9a88b57
Colin Osborn <colin.osborn@gmail.com>
2016-04-29 12:06:43 -0600

    adding conditionals

--- web_guesser.rb
+++ web_guesser.rb
@@ -3,5 +3,19 @@ require "sinatra/reloader"
 
 get "/" do
   number = rand(10)
-  erb :index, :locals => {:number => number}
-end
+    response = params["guess"].to_i
+    message = responder(response, number)
+    erb :index, :locals => {:number => number, :message => message}
+  end
+
+  # SECRET_NUMBER = rand(10)
+
+  def responder(response, number)
+    if response.to_i > number
+      "Too High!"
+    elsif response.to_i < number
+      "Too Low!"
+    else response.to_i == number
+      "You got it right!"
+    end
+  end
