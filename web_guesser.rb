4b3b15d88629926015ee594b6eb537656311920e
Colin Osborn <colin.osborn@gmail.com>
2016-05-03 20:10:36 -0600

    I3: number guessing game complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -8,14 +8,14 @@ get "/" do
     erb :index, :locals => {:number => number, :message => message}
   end
 
-  # SECRET_NUMBER = rand(10)
+  SECRET_NUMBER = rand(10)
 
   def responder(response, number)
-    if response.to_i > number
+    if response.to_i > SECRET_NUMBER
       "Too High!"
-    elsif response.to_i < number
+    elsif response.to_i < SECRET_NUMBER
       "Too Low!"
-    else response.to_i == number
+    else response.to_i == SECRET_NUMBER
       "You got it right!"
     end
   end
